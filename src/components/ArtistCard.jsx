c694909c0e7cdb777a14ee18fd64a49382debbed
Sithum Dilshan <shanukasithum28@gmail.com>
2023-06-09 12:01:42 +0530

    Update ArtistCard File
    Bug Fixed
    
    Co-Authored-By: Madura Prasad <85684994+Madura-Prasad@users.noreply.github.com>

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 const ArtistCard = ({ track }) => {
   const navigate = useNavigate();
 
+  
   return (
     <div
       className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer"
