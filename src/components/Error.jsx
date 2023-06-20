7abf15ee60ec6140791b5c3d24736a95651835ee
Sithum Dilshan <shanukasithum28@gmail.com>
2023-06-20 21:14:05 +0530

    update error file

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Error = () => (
   <div className="w-full flex justify-center items-center">
-    <h1 className="font-bold text-2xl text-white">Something went wrong. Please try again</h1>
+    <h1 className="font-bold text-2xl text-white">Something Went wrong. Please try again</h1>
   </div>
 );
 
